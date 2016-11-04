1caaede19465d8b1161de2cbcd645d7e2b5aace3
Brian Faust <hello@brianfaust.de>
2016-11-04 12:05:41 +0200

    first

---
+++ src/ServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BrianFaust\Mailjet;
+
+use GuzzleHttp\Client as HttpClient;
+use Illuminate\Support\Arr;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        app('swift.transport')->extend('mailjet', function () {
+            $config = $this->app['config']->get('services.mailjet', []);
+
+            return new MailjetTransport(
+                new HttpClient(Arr::get($config, 'guzzle', [])),
+                $config['public'], $config['private']
+            );
+        });
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        //
+    }
+}
